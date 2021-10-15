628db68528a45098778658d350e9a1dba50a7276
guillermo <guillermo@guillermo-hplaptop15bs1xx>
2021-10-15 16:17:57 +0200

    Creacion de maquinas y HashMap para las maquinas

--- src/VendingSystem.java
+++ src/VendingSystem.java
@@ -1,12 +1,29 @@
+import java.util.*;
 
 public class VendingSystem {
-	
+
+	public HashMap<Integer, VendingMachine> maquinas = new HashMap<Integer, VendingMachine>();
+
 	public VendingSystem() {
-		
+
 	}
-	public void NuevaMaquina() {
-		
-		
+
+	public void ListaMaquinas() {
+
+	}
+
+	public void NuevaMaquina(int id) {
+		if (maquinas.containsKey(id)) {
+
+		} else {
+			VendingMachine maquina = new VendingMachine(id);
+			maquinas.put(id, maquina);
+		}
+
+	}
+
+	public void BorrarMaquina(int clave) {
+		maquinas.remove(clave);
 	}
 
 }

10c609fd955054d934dbbbe053d1e24eed845f09
guill <guill@192.168.1.49>
2021-12-18 01:31:00 +0100

    VendingSystem solo falta comentar

--- src/es/uva/poo/practica/VendingSystem.java
+++ src/es/uva/poo/practica/VendingSystem.java
@@ -13,8 +13,8 @@ public class VendingSystem {
 			throw new IllegalArgumentException("La lista es nula");
 		} else {
 			for (VendingCity sede : sedes) {
-				if (!redprovincias.containsKey(sede.getProvincia())) {
-					redprovincias.put(sede.getProvincia(), sede);
+				if (!redprovincias.containsKey(sede.getProvincia().toLowerCase())) {
+					redprovincias.put(sede.getProvincia().toLowerCase(), sede);
 				}
 			}
 		}
@@ -23,32 +23,32 @@ public class VendingSystem {
 	public void crearSede(String provincia, List<VendingMachine> maquinas) {
 		if (provincia == null) {
 			throw new IllegalArgumentException("El codigo de la provincia es nulo");
-		} else if (redprovincias.containsKey(provincia)) {
+		} else if (redprovincias.containsKey(provincia.toLowerCase())) {
 			throw new IllegalArgumentException("Ya existe una provincia con ese codigo");
 		} else {
-			redprovincias.put(provincia, new VendingCity(maquinas, provincia));
+			redprovincias.put(provincia.toLowerCase(), new VendingCity(maquinas, provincia.toLowerCase()));
 		}
 	}
 
 	public void borrarSede(String provincia) {
-		if (redprovincias.containsKey(provincia)) {
-			redprovincias.remove(provincia);
+		if (redprovincias.containsKey(provincia.toLowerCase())) {
+			redprovincias.remove(provincia.toLowerCase());
 		} else {
 			throw new IllegalArgumentException("El codigo de la provincia no esta en la red");
 		}
 	}
 
 	public int numMaquinas(String provincia) {
-		if (comprobarSede(provincia)) {
-			return redprovincias.get(provincia).maquinasTotales();
+		if (comprobarSede(provincia.toLowerCase())) {
+			return redprovincias.get(provincia.toLowerCase()).maquinasTotales();
 		} else {
 			return 0;
 		}
 	}
 
 	public List<VendingMachine> listaMaquinasSede(String provincia) {
-		if (comprobarSede(provincia)) {
-			return redprovincias.get(provincia).listaMaquinas();
+		if (comprobarSede(provincia.toLowerCase())) {
+			return redprovincias.get(provincia.toLowerCase()).listaMaquinas();
 		} else {
 			return new ArrayList<>();
 		}
@@ -72,13 +72,22 @@ public class VendingSystem {
 	}
 
 	public boolean comprobarSede(String provincia) {
-		if (redprovincias.size() == 0) {
-			throw (new IllegalArgumentException("No hay ninguna sede creada"));
-		} else if (!redprovincias.containsKey(provincia)) {
-			throw (new IllegalArgumentException("No existe dicha sede"));
-		} else {
+		if (!redprovincias.containsKey(provincia.toLowerCase())) {
+			return false;
+		} 
 			return true;
+	}
+	
+	public VendingCity getSede(String provincia) {
+		if(provincia == null) {
+			throw (new IllegalArgumentException("Parametro introducido nulo"));
 		}
+		
+		if(!redprovincias.containsKey(provincia.toLowerCase())) {
+			throw (new IllegalArgumentException("No hay ninguna sede creada en esa provincia"));
+		}
+		VendingCity sedeseleccionada = redprovincias.get(provincia.toLowerCase());
+		return sedeseleccionada;
 	}
 	
 }
--- test/es/uva/poo/practica/VendingSystemTest.java
+++ test/es/uva/poo/practica/VendingSystemTest.java
@@ -5,35 +5,119 @@ import static org.junit.Assert.*;
 import java.util.*;
 
 import org.junit.Before;
-import org.junit.BeforeClass;
 import org.junit.Test;
 
 public class VendingSystemTest {
-	
+
 	private List<VendingCity> sedes = new ArrayList<>();
 	private List<VendingMachine> maquinas = new ArrayList<>();
+	private VendingSystem vs;
 
 	@Before
 	public void inizializar() {
 		maquinas.add(new VendingMachine(0, 3, 1));
 		sedes.add(new VendingCity(maquinas, "Valladolid"));
+		vs = new VendingSystem(sedes);
 	}
-	
-	
+
 	@Test
 	public void testVendingSystem() {
 		sedes.add(new VendingCity(maquinas, "Valladolid"));
-		VendingSystem vs = new VendingSystem(sedes);
+		vs = new VendingSystem(sedes);
 		assertNotNull(vs);
 		assertTrue(vs.comprobarSede("Valladolid"));
 		assertEquals(1, vs.numProvincias());
 	}
-	
+
 	@Test(expected = IllegalArgumentException.class)
 	public void testVendingSystemNulo() {
 		new VendingSystem(null);
 	}
-	
-	
 
+	@Test
+	public void testCrearSede() {
+		assertNotNull(vs);
+		vs.crearSede("Palencia", maquinas);
+		assertNotNull(vs.getSede("Palencia"));
+		assertEquals(1, vs.numMaquinas("Palencia"));
+	}
+
+	@Test(expected = IllegalArgumentException.class)
+	public void testCrearSedeProvinciaNulo() {
+		assertNotNull(vs);
+		vs.crearSede(null, maquinas);
+	}
+
+	@Test(expected = IllegalArgumentException.class)
+	public void testCrearSedeExistente() {
+		assertNotNull(vs);
+		vs.crearSede("Valladolid", maquinas);
+	}
+
+	@Test
+	public void testBorrarSede() {
+		assertNotNull(vs);
+		vs.borrarSede("Valladolid");
+		assertEquals(0, vs.numProvincias());
+		assertFalse(vs.comprobarSede("Valladolid"));
+	}
+
+	@Test(expected = IllegalArgumentException.class)
+	public void testBorrarSedeNoExiste() {
+		assertNotNull(vs);
+		vs.borrarSede("Valladolid");
+		vs.borrarSede("Valladolid");
+	}
+
+	@Test
+	public void testNumMaquinas() {
+		assertEquals(1, vs.numMaquinas("Valladolid"));
+		vs.borrarSede("Valladolid");
+		assertEquals(0, vs.numMaquinas("Valladolid"));
+	}
+
+	@Test
+	public void testListaMaquinasSede() {
+		assertEquals(1, vs.listaMaquinasSede("Valladolid").size());
+		assertNotNull(vs.listaMaquinasSede("Valladolid"));
+		vs.borrarSede("Valladolid");
+		assertEquals(0, vs.listaMaquinasSede("Valladolid").size());
+	}
+	
+	@Test
+	public void testNumProvincia() {
+		assertEquals(1, vs.numProvincias());
+	}
+	
+	@Test
+	public void testListaNomProvincia() {
+		List<String> lista = vs.listaNomProvincias();
+		assertEquals(1, lista.size());
+		assertEquals("valladolid", lista.get(0));
+	}
+
+	@Test
+	public void testListaMaqProvincias() {
+		Map<String, Integer> mapa = vs.listaMaqProvincias();
+		int i = mapa.get("valladolid");
+		assertEquals(1, i);
+		assertEquals(1, mapa.size());
+	}
+
+	@Test
+	public void testGetSede() {
+		assertNotNull(vs.getSede("Valladolid"));
+		assertEquals(1, vs.getSede("Valladolid").maquinasTotales());
+	}
+
+	@Test(expected = IllegalArgumentException.class)
+	public void testGetSedeStringNulo() {
+		vs.getSede(null);
+	}
+	
+	@Test(expected = IllegalArgumentException.class)
+	public void testGetSedeNoExiste() {
+		vs.getSede("Palencia");
+	}
+	
 }
